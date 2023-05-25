5aef8f1523023362e65b81687e617cddf09eb874
arjunn881 <arjunn881@gmail.com>
2023-05-25 17:16:01 +0530

    25/05 17:15

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,15 @@
-
-import './App.css';
-import { Navbar } from './components/navbar/Navbar';
-import { BrowserRouter as Router } from 'react-router-dom';
-
+import "./App.css";
+import { AllRoutes } from "./AllRoutes";
+import { Navbar } from "./components/navbar/Navbar";
+import { BrowserRouter as Router } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      
       <Router>
-      <Navbar/>
+        <Navbar />
+        <AllRoutes/>
       </Router>
-    
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import { Jwt } from 'jsonwebtoken';
 
 const app = express();
 
