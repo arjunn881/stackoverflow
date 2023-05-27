12f2ea96221c34f27b6874e84263f8a51d9bdd17
arjunn881 <arjunn881@gmail.com>
2023-05-27 15:41:35 +0530

    27/05 15:41

--- client/src/components/rightbar/WidgetTags.jsx
+++ client/src/components/rightbar/WidgetTags.jsx
@@ -1,7 +1,39 @@
-import React from 'react'
+import React from "react";
 
 export const WidgetTags = () => {
+  const tags = [
+    "c",
+    "html",
+    "css",
+    "javascript",
+    "jquery",
+    "reactjs",
+    "mongodb",
+    "firebase",
+    "express",
+    "mysql",
+    "nodejs",
+    "java",
+    "python",
+    "php",
+    "angular",
+    "nextjs",
+    "nestjs",
+    "c++",
+    "dsa",
+    "laravel",
+  ];
   return (
-    <div>WidgetTags</div>
-  )
-}
+    <div className="widget-tags">
+        <h4>Watched Tags</h4>
+
+        <div className="widget-tags-div">
+            {
+                tags.map((tag)=>(
+                    <p key={tag}>{tag}</p>
+                ))
+            }
+        </div>
+    </div>
+  );
+};
