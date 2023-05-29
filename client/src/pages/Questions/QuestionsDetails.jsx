6241c8b5182b1bb29032044da48eda500f0e9f2b
arjunn881 <arjunn881@gmail.com>
2023-05-29 17:02:18 +0530

    29/05 17:02

--- client/src/pages/Questions/QuestionsDetails.jsx
+++ client/src/pages/Questions/QuestionsDetails.jsx
@@ -3,71 +3,71 @@ import { useParams } from "react-router-dom";
 import upvotes from "../../asset/sort-up.svg";
 import downvotes from "../../asset/sort-down.svg";
 
+var questionsList = [
+  {
+    _id: "1",
+    upVotes: 3,
+    downVotes: 2,
+    noOfAnswers: 2,
+    questionTitle: "What is a function?",
+    questionBody: "It meant to be",
+    questionTags: ["java", "node js", "react js", "mongo db", "express js"],
+    userPosted: "mano",
+    userId: 1,
+    askedOn: "jan 1",
+    answer: [
+      {
+        answerBody: "Answer",
+        userAnswered: "kumar",
+        answeredOn: "jan 2",
+        userId: 2,
+      },
+    ],
+  },
+  {
+    _id: "2",
+    upVotes: 3,
+    downVotes: 2,
+    noOfAnswers: 0,
+    questionTitle: "What is a function?",
+    questionBody: "It meant to be",
+    questionTags: ["javascript", "R", "python"],
+    userPosted: "mano",
+    askedOn: "jan 1",
+    userId: 1,
+    answer: [
+      {
+        answerBody: "Answer",
+        userAnswered: "kumar",
+        answeredOn: "jan 2",
+        userId: 2,
+      },
+    ],
+  },
+  {
+    _id: "3",
+    upVotes: 3,
+    downVotes: 2,
+    noOfAnswers: 0,
+    questionTitle: "What is a function?",
+    questionBody: "It meant to be",
+    questionTags: ["javascript", "R", "python"],
+    userPosted: "mano",
+    askedOn: "jan 1",
+    userId: 1,
+    answer: [
+      {
+        answerBody: "Answer",
+        userAnswered: "kumar",
+        answeredOn: "jan 2",
+        userId: 2,
+      },
+    ],
+  },
+];
+
 export const QuestionsDetails = () => {
   const { id } = useParams();
-
-  var questionsList = [
-    {
-      _id: "1",
-      upVotes: 3,
-      downVotes: 2,
-      noOfAnswers: 2,
-      questionTitle: "What is a function?",
-      questionBody: "It meant to be",
-      questionTags: ["java", "node js", "react js", "mongo db", "express js"],
-      userPosted: "mano",
-      userId: 1,
-      askedOn: "jan 1",
-      answer: [
-        {
-          answerBody: "Answer",
-          userAnswered: "kumar",
-          answeredOn: "jan 2",
-          userId: 2,
-        },
-      ],
-    },
-    {
-      _id: "2",
-      upVotes: 3,
-      downVotes: 2,
-      noOfAnswers: 0,
-      questionTitle: "What is a function?",
-      questionBody: "It meant to be",
-      questionTags: ["javascript", "R", "python"],
-      userPosted: "mano",
-      askedOn: "jan 1",
-      userId: 1,
-      answer: [
-        {
-          answerBody: "Answer",
-          userAnswered: "kumar",
-          answeredOn: "jan 2",
-          userId: 2,
-        },
-      ],
-    },
-    {
-      _id: "3",
-      upVotes: 3,
-      downVotes: 2,
-      noOfAnswers: 0,
-      questionTitle: "What is a function?",
-      questionBody: "It meant to be",
-      questionTags: ["javascript", "R", "python"],
-      userPosted: "mano",
-      askedOn: "jan 1",
-      userId: 1,
-      answer: [
-        {
-          answerBody: "Answer",
-          userAnswered: "kumar",
-          answeredOn: "jan 2",
-          userId: 2,
-        },
-      ],
-    },
-  ];
 
   return (
     <div className="question-details-page">
@@ -79,7 +79,7 @@ export const QuestionsDetails = () => {
             .filter((question) => question._id === id)
             .map((question) => (
               <div className="" key={question._id}>
-                <h4>{question.questionTitle}</h4>
+                <h1>{question.questionTitle}</h1>
               </div>
             ))}
         </>
