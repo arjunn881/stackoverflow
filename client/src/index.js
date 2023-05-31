1bbfd07e1d52fd12267616e6794693eac825c02f
arjunn881 <arjunn881@gmail.com>
2023-05-31 21:59:51 +0530

    31/05 22:00

--- client/src/index.js
+++ client/src/index.js
@@ -2,11 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk'
+import { configureStore } from '@reduxjs/toolkit';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const store = configureStore({ Reducers, middleware: [thunk] });
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>
 );
-
