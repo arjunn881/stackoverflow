63943e58f842cd284b8ab4f68c74bd973b7965c3
arjunn881 <arjunn881@gmail.com>
2023-06-10 14:38:57 +0530

    10/06 14:38

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,18 +1,17 @@
-import React from 'react'
-import { Leftbar } from '../../components/leftbar/Leftbar'
-import { HomeMain } from '../../components/homemain/HomeMain'
-import { Rightbar } from '../../components/rightbar/Rightbar'
-import '../Home/Home.css';
-
+import React from "react";
+import { Leftbar } from "../../components/leftbar/Leftbar";
+import { HomeMain } from "../../components/homemain/HomeMain";
+import { Rightbar } from "../../components/rightbar/Rightbar";
+import "./Home.css";
 
 export const Home = () => {
   return (
     <div className="home-container-1">
-      <Leftbar/>
+      <Leftbar />
       <div className="home-container-2">
-      <HomeMain/>
-      <Rightbar/>
+        <HomeMain />
+        <Rightbar />
       </div>
     </div>
-  )
-  }
+  );
+};
