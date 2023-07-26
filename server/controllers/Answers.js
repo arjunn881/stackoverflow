10b19f8959b130a4f88c2ed2c2429e302afd46f5
arjunn881 <arjunn881@gmail.com>
2023-07-26 08:15:23 +0530

    26/07 08:15

---
+++ server/controllers/Answers.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+import Questions from "../models/Questions.js";
+
+export const postAnswer = async(req,res)=>{
+    const { id:_id } = req.params;
+
+    const { noOfAnswers, answerBody, userAnswered } = req.body;
+
+    if(!mongoose.Types.ObjectId.isValid(_id)){
+        return res.status(404).send('question unavailable...');
+    }
+
+    try {
+        const updatedQuestion = await Questions.findByIdAndUpdate( _id, { $addToSet: {'answer': [{answerBody,userAnswered, userId : req.userId}]}} )
+        res.status(200).json(updatedQuestion)
+    } catch (error) {
+       res.status(400).json(error);
+    }
+}
+
+const updateNoOfAnswers = async( _id, noOfAnswers)=>{
+    try {
+        await Questions.findByIdAndUpdate( _id, {$set : {'noOfAnswers' : noOfAnswers}})
+    } catch (error) {
+        console.log(error);
+    }
+}
--- server/index.js
+++ server/index.js
@@ -5,6 +5,9 @@ import cors from "cors";
 
 import userRoutes from './routes/users.js'
 import questionRoutes from './routes/Questions.js';
+import answerRoutes from './routes/Answers.js';
+
+
 const app = express();
 
 app.use(express.json({ limit: "30mb", extended: true }));
@@ -19,6 +22,7 @@ app.get("/", (req, res) => {
 
 app.use('/user', userRoutes);
 app.use('/questions', questionRoutes);
+app.use('/answer', answerRoutes)
 
 
 
